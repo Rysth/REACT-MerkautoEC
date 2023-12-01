7188bbd3c0950c93fa2ee45e38f561752d165c45
Rysth <johnpalacios.t@gmail.com>
2023-12-01 09:43:35 -0500

    Finish login page mobile version.

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -2,22 +2,22 @@ import VehicleImage from '../../assets/images/vehicles/vehicle_1.jpg';
 
 function Login() {
   return (
-    <section className="w-full bg-white">
-      <div className="grid grid-cols-2">
-        <picture className="h-full">
+    <section className="w-full h-full bg-white">
+      <div className="relative grid items-center justify-center h-full sm:grid-cols-2">
+        <picture className="absolute top-0 bottom-0 left-0 right-0 h-full sm:relative">
           <img
             src={VehicleImage}
             alt="Portrait vehicle"
-            className="object-cover w-full h-full max-h-screen "
+            className="object-cover w-full h-full max-h-screen brightness-50 sm:brightness-100"
           />
         </picture>
-        <article className="flex flex-col items-center justify-center">
+        <article className="relative z-20 flex flex-col items-center justify-center p-6 bg-white sm:bg-none rounded-xl ">
           <header>
-            <h2 className="text-2xl font-bold sm:text-4xl md:text-5xl">
+            <h2 className="text-3xl font-bold sm:text-4xl md:text-5xl">
               Iniciar Sesión
             </h2>
           </header>
-          <form className="grid gap-5 mt-5 text-sm sm:mt-10 w-60">
+          <form className="grid gap-5 mt-5 text-xs sm:text-sm sm:mt-10 w-60">
             <fieldset>
               <label
                 htmlFor="email"
@@ -46,7 +46,7 @@ function Login() {
                 />
               </label>
             </fieldset>
-            <fieldset className="flex items-center justify-end gap-1 mt-auto">
+            <fieldset className="flex items-center justify-end gap-1">
               <button
                 type="button"
                 className="float-right w-full !p-2 btn btn-info"
