fa2d28d8da1c850140403a988988e59f48e4a3dd
Rysth <johnpalacios.t@gmail.com>
2024-03-25 20:37:14 -0500

    Migrate CRA to Vite.

---
+++ src/components/ProtectedRoute/ProtectedRoute.jsx
@@ -0,0 +1,17 @@
+import { Navigate } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+function ProtectedRoute({ isAllowed, children, redirectTo }) {
+	if (!isAllowed) {
+		return <Navigate to={redirectTo} />;
+	}
+	return children;
+}
+
+ProtectedRoute.propTypes = {
+	isAllowed: PropTypes.bool.isRequired,
+	children: PropTypes.object.isRequired,
+	redirectTo: PropTypes.string.isRequired,
+};
+
+export default ProtectedRoute;
--- src/redux/slices/loginDataSlice.js
+++ src/redux/slices/loginDataSlice.js
@@ -1,64 +1,52 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { NotificationManager } from 'react-notifications';
 
 const activeStatusFromSession = localStorage.getItem('active');
 
 const credentials = {
-  email: 'admin@merkautoec.com',
-  password: '@MerkautoEC',
-  active: activeStatusFromSession === 'true',
+	email: 'admin@merkautoec.com',
+	password: '@MerkautoEC',
+	active: activeStatusFromSession === 'true',
 };
 
-export const changeActiveStatus = createAsyncThunk(
-  'credentials/changeActiveStatus',
-  async (payload) => {
-    NotificationManager.info('Autentificando...', 'Información', 1500);
-    await new Promise((resolve) => setTimeout(resolve, 1500));
+export const changeActiveStatus = createAsyncThunk('credentials/changeActiveStatus', async (payload) => {
+	await new Promise((resolve) => setTimeout(resolve, 1500));
 
-    const { email, password } = payload;
-    if (email !== credentials.email || password !== credentials.password) {
-      NotificationManager.error(
-        '¡Email o Contraseña incorrecta!',
-        'Fallo',
-        1500,
-      );
-      return { ...credentials, active: false };
-    }
+	const { email, password } = payload;
+	if (email !== credentials.email || password !== credentials.password) {
+		return { ...credentials, active: false };
+	}
 
-    NotificationManager.success('¡Ingreso Exítoso!', 'Exíto', 1500);
-    localStorage.setItem('active', true);
-    return { ...credentials, active: true };
-  },
-);
+	localStorage.setItem('active', true);
+	return { ...credentials, active: true };
+});
 
 const initialState = {
-  userCredentials: credentials,
-  loading: false,
+	userCredentials: credentials,
+	loading: false,
 };
 
 export const loginDataSlice = createSlice({
-  name: 'credentials',
-  initialState,
-  reducers: {
-    logoutFromApp: (state) => {
-      state.userCredentials.active = false;
-      localStorage.setItem('active', state.userCredentials.active);
-      NotificationManager.info('¡Muchas Gracias!', 'Información', 1500);
-    },
-  },
-  extraReducers: (builder) => {
-    builder
-      .addCase(changeActiveStatus.pending, (state) => {
-        state.loading = true;
-      })
-      .addCase(changeActiveStatus.fulfilled, (state, action) => {
-        state.loading = false;
-        state.userCredentials = action.payload;
-      })
-      .addCase(changeActiveStatus.rejected, (state) => {
-        state.loading = false;
-      });
-  },
+	name: 'credentials',
+	initialState,
+	reducers: {
+		logoutFromApp: (state) => {
+			state.userCredentials.active = false;
+			localStorage.setItem('active', state.userCredentials.active);
+		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(changeActiveStatus.pending, (state) => {
+				state.loading = true;
+			})
+			.addCase(changeActiveStatus.fulfilled, (state, action) => {
+				state.loading = false;
+				state.userCredentials = action.payload;
+			})
+			.addCase(changeActiveStatus.rejected, (state) => {
+				state.loading = false;
+			});
+	},
 });
 
 export const loginDataActions = loginDataSlice.actions;
