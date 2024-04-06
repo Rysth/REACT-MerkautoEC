7f72349fb54f09fa9f953489a9876745e82bd1d1
Rysth <johnpalacios.t@gmail.com>
2024-04-06 10:03:46 -0500

    Change basename

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig(() => {
 	return {
-		base: 'https://www.asvesot.com/apptaller.asvesot.com',
+		base: 'https://www.asvesot.com/merkautoec',
 		build: {
 			outDir: 'build',
 		},
